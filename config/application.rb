f8de07c30e9b80a9c33658a7ecfc4835708d103e
scarletrunner7000 <scarletrunner7000@gmail.com>
2015-03-16 12:05:28 +0900

    [develop] Bootstrap の導入
    Bootstrap を導入した。
    ただ、今後は最低限のことを除いてデザインのことはできるだけ考えない。後回し。
    
    【参考】
    https://www.imd-net.com/column/2760/

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module TextbookAuc
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Added for bootstrap
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
